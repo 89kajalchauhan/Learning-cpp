3127e14e87842ffea1ea7bafdaf23a404767e541
89kajalchauhan <tumhara-github-email@example.com>
2026-01-18 22:33:45 +0530

    Add first C++ program

---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include <iostream> 
+using namespace std;
+int main() {
+   /* cout << "Namaste duniya" <<endl; 
+   int a = 2.0/5;
+   cout << a <<endl;
+
+   cout << 2.0/5 << endl;. */
+
+   /* int a = 2;
+    int b = 3;
+
+   bool first = (a == b);
+   cout << first << endl;
+      
+    bool second = (a <= b);
+    cout<< second << endl;
+
+     bool third = (a >= b);
+    cout<< third << endl;
+
+     bool fourth = (a != b);
+    cout<< fourth << endl; 
+
+     int a = 21;
+     cout << !a <<endl; */
+
+
+    /*if-else , while loop*/
+
+   //int a ;
+   //cin>>a;
+
+   /*cout<<"Value of n is:" << n <<endl; */
+
+
+  /*if(a>0) {
+    cout << "A is positive" <<endl;
+  }
+
+  else{
+    cout<< "A is negative" <<endl;
+  } */
+
+   /*int a , b;
+
+   cin>> a>> b;
+
+   cout<<"value of a and b is "  <<a  <<b <<endl; */
+
+  /*this is the progrm that tells which num is greater*/
+   
+  /*int a , b;
+  cout<<"Enter the value of a" <<endl;
+
+ cin>>a;
+  cout<<"Enter the value of b" <<endl;
+  cin>>b;
+  
+  if(a>b) {
+    cout<<"A is greater" <<endl;
+
+  } 
+  
+  if(b>a) {
+    cout<<"B is greater" <<endl; 
+  } */
+  
+
+
+  /*this is the program t check wheather the num is  +ve -ve or zero and 
+  i write it by myself*/
+
+  /*int a;
+
+  cout<<"Enter the value of a" <<endl;
+  cin>>a;
+
+ 
+  if(a>0) {
+    cout<<"A is positive num" <<endl;
+
+  }
+  
+  if(a<0) {
+    cout<<"A is negative num" <<endl;
+  }
+
+  if(a==0) {
+    cout<<"A is zero" <<endl;
+  } */
+
+    return 0;
+}
