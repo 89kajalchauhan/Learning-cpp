a53372359acf76a64c81c2871f85802c7b3ae5d8
89kajalchauhan <tumhara-github-email@example.com>
2026-01-27 20:46:44 +0530

    Added C++ practice code (bitwise, loops, scope)

---
+++ bo.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+/*int main() {
+      int a = 4;
+      int b = 5;
+
+      cout<< " a&b " << (a & b) <<endl;
+      cout<< " a|b " << (a | b) <<endl;
+      cout<< " ~a " << (~a) <<endl;
+      cout<< " a^b " << (a ^ b ) <<endl;
+
+      cout<< (17>>1) <<endl; 
+      cout<< (17>>2) <<endl;
+      cout<< (19<<1) <<endl;
+      cout<< (19<<2) <<endl;
+
+      int i = 7;
+
+      cout<< (++i) <<endl;
+      cout<< (i++) <<endl;
+      cout<< (--i)  <<endl;
+      cout<< (i--) <<endl;
+
+   /* int x = 1;
+     int y = a++;
+     int z = ++a;
+
+     cout<< y<<endl;
+     cout<< z<<endl;*/
+
+       /*int n;
+       cout<<"enter the value of n" <<endl;
+       cin>>n;
+    
+       cout<<"printing count from 1 to n" <<endl;
+    
+      for(int i = 1; i<=n; i++) {
+        cout<< i  <<endl;
+}
+    return 0;
+}*/
+
+
+int main() {
+  /*8int n;
+  cout<<"enter the value of n" <<endl;
+  cin>>n;
+
+  int sum = 0;
+
+  for(int i = 1; i<=n; i++){
+    sum = sum + i;
+  }
+  cout<< sum <<endl;*/
+
+  /*int n = 20;
+
+ int a = 0;
+ int b = 1;
+  
+ for(int i = 1; i<=n; i++){
+     int nextNumber = a+b;
+     cout<<nextNumber<<endl;
+
+     a = b;
+     b = nextNumber;
+ }*/
+
+  /*int n;
+  cout<<"enter the value of n" <<endl;
+  cin >>n;
+
+  bool isPrime = 1;
+
+  for(int i = 2; i<n; i++) {
+     
+    if(n%i == 0) {
+      isPrime = 0;
+      break;
+    }
+  }
+   if(isPrime == 0 ) {
+    cout<<"not a prime number"<<endl;
+
+   }
+   else{
+    cout<<"is a prime number"<<endl;
+   }*/
+
+
+   /*for(int i = 0; i<5; i++) {
+
+       cout<< "hiiiiii"  << endl;
+       cout<< "helloooww"  <<endl;
+
+       continue;
+       
+       cout<< "reply to krdeee" << endl;
+   }*/
+
+   int a = 3;
+
+   cout<< a <<endl;
+
+   if(true) {
+    int a = 5;
+    cout << a <<endl;
+
+   }
+
+
+
+
+
+
+
+
+}
