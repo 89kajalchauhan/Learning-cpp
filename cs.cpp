0c87e7d1c2831a32d0209983d480a6a2b31172d4
Kajal Chauhan <kajalchauhan892079@gmail.com>
2026-02-08 22:22:45 +0530

    Add C++ practice programs

---
+++ cs.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    /*int n;
+    cin>>n;
+
+    if (n >= 18) {
+        cout<<"person is eligible to vote"<<endl;
+    }
+    else {
+        cout<<"person is not eligible to vote"<<endl;
+    }*/
+
+    /*int n;
+    cin>>n;
+
+
+    if(n%2 == 0) {
+        cout<<"number is even" <<endl;
+    }
+     
+    else{
+        cout<<"number is odd" <<endl;
+
+    }*/
+
+    /*int marks;
+    cin>>marks;
+
+    if (marks >= 90) {
+        cout<<"grade A"<<endl;
+    }
+    else if (marks >= 80) {
+        cout<<"grade B"<<endl;
+    }
+    else if (marks >= 70) {
+        cout<<"grade C"<<endl;
+    }
+    else if (marks >= 60) {
+        cout<<"grade D"<<endl;
+    }
+    else {
+        cout<<"grade F"<<endl;
+    }*/
+
+    /*char ch;
+    cin>>ch;
+    if(ch <= 'z' && ch >= 'a') {
+        cout<<"lowercase"<<endl;
+    }
+    else if(ch <= 'Z' && ch >= 'A') {
+        cout<<"uppercase"<<endl;
+    }
+    else if(ch <= '9' && ch >= '0') {
+        cout<<"digit"<<endl;
+    }
+    else {
+        cout<<"special character"<<endl;
+    }
+    return 0;*/
+   
+
+
+    int n = 45;
+
+    cout<<(n >= 0 ? "positive" : "negative")<<endl;
+    
+    return 0;
+   
+
+
+
+
+
+
+
+
+}
+    
+
+     
