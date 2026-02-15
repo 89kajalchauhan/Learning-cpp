5da499eeb65a3e7967fa9061d612ee85e2dfb039
Kajal Chauhan <kajalchauhan892079@gmail.com>
2026-02-15 23:33:38 +0530

    Added code.css and updated project files

---
+++ code.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    /*int a = 10, b = 20;
+    cout<< (a << b ) << endl; // bitwise or*/
+
+
+    cout<< (10 & 20) << endl; // bitwise and
+
+    cout<< (20 | 10) << endl; // bitwise pr 
+
+    cout<< ( 6 & 5) << endl; // bitwise and
+
+    cout<< (6 | 5) << endl; // bitwise or
+
+    cout<< (6 ^ 5) << endl; // bitwise xor
+
+    cout << (6 >> 1) << endl; // right shift
+
+    cout << (6 << 1) << endl; // left shift
+
+    cout << ( 5 - 2 * 6) << endl; // operator precedence
+
+    cout << (5 * 4 % 3) << endl; // operator precedence
+
+    cout << sizeof(int) << endl; // size of int data type
+
+    cout<< sizeof(long long) << endl; // size of long long data type
+
+     cout<< sizeof(short int) << endl; // size of float data type
+
+      cout<< sizeof(long int) << endl; // size of double data type
+
+       cout<< sizeof(signed int) << endl; // size of char data type
+    return 0;
+}
