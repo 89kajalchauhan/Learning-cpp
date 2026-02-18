3adfc6fdb364c17659a96086b63eb65ad6f8d41b
Kajal Chauhan <kajalchauhan892079@gmail.com>
2026-02-18 11:04:50 +0530

    Added C++ practice files

---
+++ array.cpp
@@ -0,0 +1,55 @@
+//Going to study arrays in C++ and how to use them
+#include <iostream>
+using namespace std;
+int main() {
+
+
+    // declearing an array of integers with size 5
+    /*int arr[5]; // this is an array of integers with size 5
+
+    //initializing the array with values 
+    arr[0] = 10; 
+    arr[1] = 20;
+    arr[3] = 30; 
+    arr[4] = 40;
+    arr[5] = 50;
+
+    //printing the values of the array
+    cout<< arr[0] << endl;  //this will print 10
+    cout<< arr[1] << endl;  //this will print 20
+    cout<< arr[2] << endl;  //this will print 0 because we have not initialized it
+    cout<< arr[3] << endl;  //this will print 30
+    cout<< arr[4] << endl;  //this will print 40
+    cout<< arr[5] << endl;  //this will print 50 but it is out of bounds and can cause undefined behavior*/
+
+    /*int marks[5] = {10, 20, 30, 40, 50};
+
+    double prices[3] = {10.5, 20.5, 30.5};*/
+
+    /*int siz = 5;
+    int marks[size];
+
+    for(int i = 0; i< size; i++) {
+        cin >> marks[i]; // this will take input from the user and store it in the array
+    }
+
+    for(int i = 0; i< size; i++) {
+        cout<< marks[i] << endl; // this will print the values of the array
+    }*/
+
+    //find the smallest and the largest of the araay//
+    
+
+    int nums[5] = {10, 20, 5, 30, 15};
+    int smallest = nums[0];
+    int largest = nums[0];
+    for(int i = 1; i < 5; i++) {
+        if(nums[i] < smallest) {
+            smallest = nums[i];
+        }
+        if(nums[i] > largest) {
+            largest = nums[i];
+        }
+    }
+
+}
